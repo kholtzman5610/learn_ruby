1851e5bbb9180f5460f4e21d7b40639dd1e042b2
kholtzman5610 <kassidy.holtzman@outlook.com>
2019-12-13 23:44:43 +0000

    completed 7 of 7 exercises

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,12 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    t = Time.new(0) + seconds
+    t.asctime.split(" ")[3]
+  end
 end
