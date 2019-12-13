5c2e2d757ef3ed615d57c6ee2eb82b69ffe721b4
kholtzman5610 <kassidy.holtzman@outlook.com>
2019-12-13 23:20:34 +0000

    completed 3 of 7 exercises

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -11,5 +11,21 @@ def sum(x)
     x.sum 
 end
 
+def multiply(*x)
+  x == [] ? total = 0 : total = 1
+  x.each{ |item| total *= item }
+  total
+end
 
+def power(x, y)
+  x ** y
+end
 
+def factorial(x)
+  tally, total = x, 1
+  x.times do  
+    total *= tally
+    tally -= 1
+  end
+  total
+end
