8b3aa0012f275352bfcb4ac7d4a91be82e9712a0
kholtzman5610 <kassidy.holtzman@outlook.com>
2019-12-13 23:39:28 +0000

    completed 5 of 7 exercises

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,34 @@
-#write your code here
+Vowels = ["a", "e", "i", "o", "u"]
+Consonant_Filter = /(\A[^aeio]{1,3})(\w*)/ 
+
+
+def translate(word) 
+  words = word.split(" ")
+  translated_words = apply_translations(words)
+  restring(translated_words)
+end
+
+private
+
+def apply_translations(array)
+  translated_words = array.map do |wrd|
+    start_with_vowel?(wrd) ? vowel_translate(wrd) : consonant_translate(wrd)
+  end
+end
+
+def restring(array)
+  output = array.inject(""){ |result, item| result << item + " "}.strip
+end
+
+def start_with_vowel?(word)
+  Vowels.include?(word[0])
+end
+
+def vowel_translate(word)
+  word + "ay"
+end
+
+def consonant_translate(word)
+  prefix = word.match(Consonant_Filter)
+  word = prefix[2] + prefix[1] + "ay"
+end
